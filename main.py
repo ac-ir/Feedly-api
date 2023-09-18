8b920120f06a9917ec93936f3ff23031405b226b
Your Name <you@example.com>
2023-09-18 11:59:23 +0000

    added more methods

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 import requests, os
 from pprint import pprint
 
+class Feedly():
 
-url = "https://cloud.feedly.com/v3/profile"
-token = os.environ.get('token')
+    def __init__(self):
+        self.url = "https://cloud.feedly.com"
+        self.token = os.environ.get('token')
 
-r = requests.get(url, headers={'Authorization': 'Bearer ' + token})
-pprint(r.json())
+    def profile(self):
+        self.endpoint ="/v3/profile"
+        self.get()
+
+    def search(self, query):
+        self.endpoint = f"/v3/search/contents?streamId=:replace_with_id&query={query}"
+        self.get()
+
+    def collections(self):
+        self.endpoint="/v3/collections"
+        self.get()
+    def get(self):
+        r = requests.get(self.url + self.endpoint, headers={'Authorization': 'Bearer ' + self.token})
+        pprint(r.json())
+
+
+if __name__ == "__main__":
+    f = Feedly()
+    f.collections()
